ef36cc3a7f578f442b26469eea5ee9ac210445e9
PutraF26 <fimasrisnayh@gmail.com>
2023-07-17 15:48:31 +0700

    Add project files.

---
+++ FinalExam/FinalExam.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    string nama;
+    int jumlah;
+    string tipe;
+    Node* next;
+
+    Node(string nama, int jumlah, string tipe) {
+        this->nama = nama;
+        this->jumlah = jumlah;
+        this->tipe = tipe;
+        this->next = NULL;
+    }
+};
+
+class ManajemenProduk {
+private:
+    Node* head;
+
+public:
+    ManajemenProduk() {
+        head = NULL;
+    }
+
+    void tambahProduk() {
+        string nama;
+        int jumlah;
+        string tipe;
+
+        cout << "========== TAMBAH PRODUK ==========" << endl;
+        cout << "Nama Produk : ";
+        getline(cin, nama);
+        cout << "Jumlah Produk : ";
+        cin >> jumlah;
+        cin.ignore(); // Membersihkan buffer input
+        cout << "Tipe Produk : ";
+        getline(cin, tipe);
+
+        Node* newNode = new Node(nama, jumlah, tipe);
+
+        if (head == NULL) {
+            head = newNode;
+        }
+        else {
+            Node* current = head;
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+
+        cout << "Produk berhasil ditambahkan!" << endl;
+    }
+
+    void tampilkanSemuaProduk() {
+        cout << "========== SEMUA DATA PRODUK ==========" << endl;
+        if (head == NULL) {
+            cout << "Belum ada data produk yang tersimpan." << endl;
+        }
+        else {
+            Node* current = head;
+            while (current != NULL) {
+                cout << "Nama Produk  : " << current->nama << endl;
+                cout << "Jumlah Produk: " << current->jumlah << endl;
+                cout << "Tipe Produk  : " << current->tipe << endl;
+                cout << "-----------------------" << endl;
+                current = current->next;
+            }
+        }
+    }
+
+    void cariProdukByNama() {
+        cout << "========== CARI PRODUK BERDASARKAN NAMA ==========" << endl;
+        if (head == NULL) {
+            cout << "Belum ada data produk yang tersimpan." << endl;
+        }
+        else {
+            string targetNama;
+            cout << "Nama Produk yang dicari: ";
+            getline(cin, targetNama);
+
+            Node* current = head;
+            int posisi = 0;
+            bool found = false;
+
+            while (current != NULL) {
+                posisi++;
+                if (current->nama == targetNama) {
+                    cout << "Data ditemukan pada posisi: " << posisi << endl;
+                    cout << "Nama Produk  : " << current->nama << endl;
+                    cout << "Jumlah Produk: " << current->jumlah << endl;
+                    cout << "Tipe Produk  : " << current->tipe << endl;
+                    found = true;
+                    break;
+                }
+                current = current->next;
+            }
+
+            if (!found) {
+                cout << "Nama produk " << targetNama << " TIDAK DITEMUKAN" << endl;
+            }
+        }
+    }
+
+    void algorithmaSortByJumlahProduk() {
+        if (head == NULL || head->next == NULL) {
+            return; // Tidak perlu sorting jika hanya terdapat 0 atau 1 node
+        }
+
+        bool swapped;
+        Node* current;
+        Node* last = NULL;
+
+        do {
+            swapped = false;
+            current = head;
+
+            while (current->next != last) {
+                if (current->jumlah < current->next->jumlah) {
+                    swap(current, current->next);
+                    swapped = true;
+                }
+                current = current->next;
+            }
+
+            last = current;
+        } while (swapped);
+    }
+
+    void swap(Node* a, Node* b) {
+        string tempNama = a->nama;
+        int tempJumlah = a->jumlah;
+        string tempTipe = a->tipe;
+
+        a->nama = b->nama;
+        a->jumlah = b->jumlah;
+        a->tipe = b->tipe;
+
+        b->nama = tempNama;
+        b->jumlah = tempJumlah;
+        b->tipe = tempTipe;
+    }
+};
+
+int main() {
+    ManajemenProduk manajemenProduk;
+    int pilihan;
+
+    do {
+        cout << "========== APLIKASI MANAJEMEN DATA PRODUK ==========" << endl;
+        cout << "1. Tambah Produk" << endl;
+        cout << "2. Tampilkan Semua Produk" << endl;
+        cout << "3. Cari Produk berdasarkan Nama" << endl;
+        cout << "4. Urutkan Produk berdasarkan Jumlah" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Pilihan: ";
+        cin >> pilihan;
+        cin.ignore();
+
+        switch (pilihan) {
+        case 1:
+            manajemenProduk.tambahProduk();
+            break;
+        case 2:
+            manajemenProduk.tampilkanSemuaProduk();
+            break;
+        case 3:
+            manajemenProduk.cariProdukByNama();
+            break;
+        case 4:
+            manajemenProduk.algorithmaSortByJumlahProduk();
+            cout << "Produk berhasil diurutkan berdasarkan jumlah." << endl;
+            break;
+        case 5:
+            cout << "Terima kasih! Program selesai." << endl;
+            break;
+        default:
+            cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+        }
+
+        cout << endl;
+    } while (pilihan != 5);
+
+    return 0;
+}
+
+
+//2.bubblesort
+//4.jika mendapat data fifo
+//5a. Dalam struktur pohon yang diberikan, kedalaman dapat dihitung dengan menghitung jumlah tingkat (level) dalam struktur pohon. Dalam gambar yang diberikan, kedalaman struktur pohon 4 
